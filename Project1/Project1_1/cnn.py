16ec5fa696564a3c830678c4af9821140e3ec48d
Thea Våge <tv@cerebriu.com>
2022-06-06 17:42:07 +0200

    test

--- Project1/Project1_1/cnn.py
+++ Project1/Project1_1/cnn.py
@@ -1,13 +1,16 @@
+import torch
+import torch.nn as nn
+
 class cnn():#insert input
     def __init__(self):
         super(self).__init__()
-        self.convolutional = (
-                ...,
-                ...,
-                ...,
-                ...)
+        self.convolutional = nn.Sequential(
+                nn.Conv2d(3,8 , kernel_size=3, padding='same'),
+                nn.ReLU(),
+                nn.Conv2d(8,16 , kernel_size=3, padding='same'),
+                nn.ReLU())
 
 
     def forward(self, x):
-        out = x
+        out = self.convolutional(x)
         return out
