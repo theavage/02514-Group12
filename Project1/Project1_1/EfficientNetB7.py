a26a5c96449a2f1d5ab21d198193355194af16b9
theavage <thea@vage.com>
2022-06-07 11:54:56 +0200

    Updated Efficientnet

--- Project1/Project1_1/EfficientNetB7.py
+++ Project1/Project1_1/EfficientNetB7.py
@@ -1,13 +1,17 @@
+from fcntl import F_SEAL_SEAL
 import torch
 from efficientnet_pytorch import EfficientNet
 import torch.nn as nn
 
-class EfficientNetB7():
+def EfficientNetB7():
 
     # OPTION 1
-    model = EfficientNet.from_name('efficientnet-b7')
-    num_ftrs = model._fc.in_features
-    model._fc = nn.Linear(num_ftrs, 1)
+    #model = EfficientNet.from_name('efficientnet-b7')
+    model = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=False)
+    num_classes =1
+    model.fc = nn.Linear(512, num_classes)
+
+    return model
 
     #OPTION 2
     #efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 
