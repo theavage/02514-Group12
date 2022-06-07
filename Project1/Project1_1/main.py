07696d3dcbdd505a117ae65a6aff9280b4d4c8ec
theavage <thea@vage.com>
2022-06-07 18:08:33 +0200

    Updated main file

--- Project1/Project1_1/main.py
+++ Project1/Project1_1/main.py
@@ -2,16 +2,8 @@ import torch
 import torch.nn as nn
 from torchmetrics import HingeLoss
 from PIL import Image
-<<<<<<< HEAD
-
 from HotDogUtils import checkDevice, loadHotDogData, loadHotDogData, showHotDogData, trainNet, saliency_map, plot_graphs
 from HotDogModels import createEfficientNetB7, cnn, createResNet50
-=======
-import numpy as np
-
-from HotDogUtils import checkDevice, loadHotDogData, loadHotDogData, showHotDogData, trainNet, saliency_map, plot_graphs
-from HotDogModels import createResNet50, cnn, createEfficientNetB7
->>>>>>> 85b4d841b15618168f721903e6060b805349a6eb
 
 SHOW_RESULTS = False
 isAugmented = False
@@ -27,12 +19,7 @@ print(model_path)
 # Check if we run on GPU
 device = checkDevice()
 
-<<<<<<< HEAD
-if (not SHOW_RESULTS):
-=======
-if (SHOW_RESULTS == False):
->>>>>>> 85b4d841b15618168f721903e6060b805349a6eb
-    
+if (SHOW_RESULTS == False):    
     # Load data
     # Check if it is augmented
     train_loader, test_loader, trainset, testset = loadHotDogData(128, 64, isAugmented)
@@ -41,7 +28,6 @@ if (SHOW_RESULTS == False):
     #showHotDogData(train_loader)
 
     # Load model
-<<<<<<< HEAD
     model = cnn
     model = model()
     model.to(device)
@@ -66,34 +52,4 @@ else:
     #LOAD IMAGE FROM COMPUTER
     image = Image.open('gpu_meme.jpg')
     saliency_map(device, image, model_path, name)
-=======
-    model = createEfficientNetB7()
-    #model = model()
-    model.to(device)
 
-
-    # Optimizers
-    sgd = torch.optim.SGD(model.parameters(), lr=0.1)
-    momentum = torch.optim.SGD(model.parameters(), lr=0.1, momentum=1) 
-    adam = torch.optim.Adam(model.parameters(), lr=0.1)
-
-    # Loss Functions
-    ce = nn.CrossEntropyLoss()
-    hinge = HingeLoss()
-
-
-    # Train model
-    model, out_dict = trainNet(model, 2, sgd, ce, train_loader, test_loader,trainset,testset, device)
-    torch.save(model, model_path)
-
-    data = out_dict.items()
-    train_data = np.asarray(list(data))
-    np.save(name_np, train_data)
-
-    plot_graphs(out_dict, name)
-
-else:
-    #LOAD IMAGE FROM COMPUTER
-    image = Image.open('gpu_meme.jpg')
-    saliency_map(device, image, model_path, name)
->>>>>>> 85b4d841b15618168f721903e6060b805349a6eb
