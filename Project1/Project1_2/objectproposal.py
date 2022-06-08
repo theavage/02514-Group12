943c2e56b8169a8e0d4b16035414e5e1068acb0a
hoffmannc <hoffmann.colin@yahoo.de>
2022-06-08 14:43:48 +0200

    proposal changes

--- Project1/Project1_2/objectproposal.py
+++ Project1/Project1_2/objectproposal.py
@@ -10,7 +10,6 @@ def createObjectProposals(image):
 
     return rectangles
 
-
 def plotProposals(image, rectangles, n):
 
     image_rectangles = image.copy()
@@ -20,18 +19,3 @@ def plotProposals(image, rectangles, n):
             image_rectangles = cv2.rectangle(image_rectangles, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
 
     cv2.imshow("Object proposals", image_rectangles)
-
-def cropAndResize(image, rectangle, size):
-
-    x, y, w, h = rectangle
-    x1, y1 = x, y
-    x2, y2, = x + w, y + h
-    cropped = image[y1:y2, x1:x2]
-    resized = cv2.resize(cropped, size)
-    
-    return resized
-
-
-image = cv2.imread('testimage.jpeg')
-rects = createObjectProposals(image)
-print(rects)
--- Project1/Project1_2/utils.py
+++ Project1/Project1_2/utils.py
@@ -9,3 +9,13 @@ def checkDevice():
         print("The code will run on CPU. Go to Edit->Notebook Settings and choose GPU as the hardware accelerator")
     
     return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def cropAndResize(image, rectangle, size):
+
+    x, y, w, h = rectangle
+    x1, y1 = x, y
+    x2, y2, = x + w, y + h
+    cropped = image[y1:y2, x1:x2]
+    resized = cv2.resize(cropped, size)
+    
+    return resized
