0edebd45caea3a855a9b03f797bc4a41aab3feb0
s212781 <104715843+s212781@users.noreply.github.com>
2022-06-11 20:58:29 +0200

    Add files via upload

--- Project1/Project1_2/dataset.py
+++ Project1/Project1_2/dataset.py
@@ -1,11 +1,6 @@
 from utils import *
 
-train_ids, val_ids, _, indices, groundtruth, classes, images = loadData()
+train_ids, val_ids, test_ids, indices, groundtruth, classes, images = loadData()
 
-#trainset = createDataSet(images, indices, classes, groundtruth, train_ids)
-valset = createDataSet(images, indices, classes, groundtruth, val_ids)
-
-#trainsetpath = '/zhome/df/9/164401/02514-Group12/Project1/Project1_2/trainset.pt'
-#torch.save(trainset, trainsetpath)
-valsetpath = '/zhome/df/9/164401/02514-Group12/Project1/Project1_2/valset.pt'
-torch.save(valset, valsetpath)
+createDataSet(images, indices, classes, groundtruth, train_ids, 'train')
+createDataSet(images, indices, classes, groundtruth, test_ids, 'val')
