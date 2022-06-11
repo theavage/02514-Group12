4d562f82669d573f7f80c98a59a021972be362e7
hoffmannc <90707703+hoffmannc@users.noreply.github.com>
2022-06-11 14:58:22 +0200

    Update objectproposal.py

--- Project1/Project1_2/objectproposal.py
+++ Project1/Project1_2/objectproposal.py
@@ -13,7 +13,6 @@ def createObjectProposals(image):
     return rectangles
 
 def edgeBoxDetection(image):
-    proposals = np.array([])
     model = '/zhome/df/9/164401/02514-Group12/Project1/Project1_2/model.yml'
     edge_detection = cv2.ximgproc.createStructuredEdgeDetection(model)
     edges = edge_detection.detectEdges(np.float32(image) / 255.0)
@@ -22,7 +21,7 @@ def edgeBoxDetection(image):
     edges = edge_detection.edgesNms(edges, orimap)
 
     edge_boxes = cv2.ximgproc.createEdgeBoxes()
-    edge_boxes.setMaxBoxes(1000)
+    edge_boxes.setMaxBoxes(5000)
     boxes = edge_boxes.getBoundingBoxes(edges, orimap)
     
     return boxes[0]
